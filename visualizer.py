409b96f1b18a75d7b5dc1bba0b07a077297656de
XabierCS <xcalle91@gmail.com>
2022-08-12 10:31:48 +0200

    Fix NaN values
    Fix problem with samples having NaN values in LRR or BAF

--- visualizer.py
+++ visualizer.py
@@ -247,6 +247,7 @@ class Window(QWidget):
       try: 
         SampleRaw = pd.DataFrame(tabix_query(pathFull,str(chrX), 0, int(250e6))) # To get the entire chr
         print('Sample with succesfull tabix file: '+ pathFull)
+	SampleRaw = SampleRaw.astype(float) # Deal with NaN by changing type to float
       except:
         print('Cannot print sample')
         print('Error file'+pathFull)
